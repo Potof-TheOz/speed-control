8189d6afdf10c131348a3a5421f37251c994f46c
julien.valery <julien.valery@the-oz.com>
2024-09-30 11:57:57 +0200

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,19 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
 );
