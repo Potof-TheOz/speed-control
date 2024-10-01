c0dd10ed8b78fdf0f8cfc9d62ab7ced7f86e1afd
Laurent-TheOz <laurent.ravix@the-oz.com>
2024-10-01 15:04:59 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,23 @@ const main: SpeedControl.mainFunction = function (
 ) {
   // code here bros!
   // force et honneur
+  //if (_turretConfiguration.maxSpeed < _sensorData.speed) {
+  //}
+
+  let legalSpeed = 0;
+
+  if (
+    _turretConfiguration.maxSpeed >= 50 &&
+    _turretConfiguration.maxSpeed <= 100
+  ) {
+    legalSpeed = _sensorData.speed - 5;
+  }
+
   return {
     speeding: false,
     delta: 0,
     legalSpeed: 0,
-    vehicleLicensePlate: '',
+    vehicleLicensePlate: _sensorData.vehicleLicensePlate,
   };
 };
 
